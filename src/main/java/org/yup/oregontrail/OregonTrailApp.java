272042968509a71d97eca56af44724ed013ed3c6
Eric Schwartz <erics273@gmail.com>
2023-04-21 12:09:35 -0400

    added wagon methods for join and total food

--- src/main/java/org/yup/oregontrail/OregonTrailApp.java
+++ src/main/java/org/yup/oregontrail/OregonTrailApp.java
@@ -16,6 +16,25 @@ public class OregonTrailApp {
         System.out.println(dwene.eat());
         System.out.println(dwene.getFood());
 
+        Traveler eric = new Traveler("Eric", 75, 127);
+
+        Wagon ourWagon = new Wagon(5);
+
+        //asking the wagon to give us the passenger list and we are checking the size (number of passengers)
+        System.out.println("number of people on the wagon: " + ourWagon.getPassengers().size());
+        //this like attempts to put dwene (or any traveler we want) into the wagon if there is room
+        System.out.println(ourWagon.join(dwene));
+        //asking the wagon to give us the passenger list and we are checking the size (number of passengers)
+        System.out.println("number of people on the wagon: " + ourWagon.getPassengers().size());
+
+
+        //this like attempts to put dwene (or any traveler we want) into the wagon if there is room
+        System.out.println(ourWagon.join(eric));
+
+
+        //this like attempts to put dwene (or any traveler we want) into the wagon if there is room
+        System.out.println(ourWagon.getTotalFood());
+
 
     }
 
--- src/main/java/org/yup/oregontrail/Wagon.java
+++ src/main/java/org/yup/oregontrail/Wagon.java
@@ -1,8 +1,52 @@
 package org.yup.oregontrail;
 
+import java.util.ArrayList;
+
 public class Wagon {
 
-    private Traveler[] passengers;
+    private ArrayList<Traveler> passengers = new ArrayList();
     private int capacity;
 
+    public Wagon(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String join(Traveler theTraveler){
+
+        if(this.capacity > this.passengers.size()){
+            this.passengers.add(theTraveler);
+            return theTraveler.getName() + " has joined the wagon!";
+        }
+
+        return "Ain't no room on this wagon for " + theTraveler.getName();
+
+    }
+
+    public String getTotalFood(){
+
+        int totalFood = 0;
+
+        for(int i=0; i < this.getPassengers().size(); i++){
+            totalFood += this.getPassengers().get(i).getFood();
+        }
+
+        return "The wagon has a total amount of food that equals: " + totalFood;
+
+    }
+
+    public ArrayList<Traveler> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(ArrayList<Traveler> passengers) {
+        this.passengers = passengers;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }
